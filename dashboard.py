a863d92ea7966879a060c50574d420ba2cd1938e
tungpham2001 <18tp12@queensu.ca>
2025-08-04 17:35:46 -0400

    added security

--- dashboard.py
+++ dashboard.py
@@ -5,8 +5,28 @@ import os
 import altair as alt
 import time
 import datetime
+import hashlib
+import os
 
 DB_PATH = os.path.join("data", "budget.db")
+PASSWORD_HASH = os.environ.get("BUDGET_TRACKER_PASSWORD_HASH")
+
+def check_password():
+    def password_entered():
+        if hashlib.sha256(st.session_state["password"].encode()).hexdigest() == PASSWORD_HASH:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("enter password", type="password", on_change=password_entered, key="password")
+        st.stop()
+    elif not st.session_state["password_correct"]:
+        st.text_input("enter password", type="password", on_change=password_entered, key="password")
+        st.error("❌ incorrect password")
+        st.stop()
+check_password()
 
 # --- Initialize Database Tables ---
 def init_db():
